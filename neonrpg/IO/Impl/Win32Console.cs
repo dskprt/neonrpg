6aba5be3f44936236a1dac094fdd75a252b53860
dskprt <59660013+dskprt@users.noreply.github.com>
2020-12-16 09:59:37 +0100

    cleanup

--- neonrpg/IO/Impl/Win32Console.cs
+++ neonrpg/IO/Impl/Win32Console.cs
@@ -1,64 +1,60 @@
-﻿using Microsoft.Win32.SafeHandles;
-using neonrpg.Utilities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace neonrpg.IO.Impl {
 
     class Win32Console : NeonConsole {
 
-        private const string CLEAR_SEQUENCE = "\u001b[2J";
-        private Win32.CHAR_INFO EMPTY_CHAR = new Win32.CHAR_INFO() {
+        private CHAR_INFO EMPTY_CHAR = new CHAR_INFO() {
             Attributes = 0,
-            Char = new Win32.CHAR_UNION() {
-                AsciiChar = (byte)' ',
-                UnicodeChar = ' '
-            }
+            AsciiChar = ' ',
+            UnicodeChar = ' '
         };
 
-        private Win32.CHAR_INFO[] Buffer { get; set; }
-        private Win32.CHAR_INFO[] PrevBuffer { get; set; }
+        private CHAR_INFO[] Buffer { get; set; }
+        private CHAR_INFO[] PrevBuffer { get; set; }
         private IntPtr Handle { get; set; }
 
-        private Win32.COORD bufferSize;
-        private Win32.COORD bufferCoord;
-        private Win32.SMALL_RECT region;
+        private COORD bufferSize;
+        private COORD bufferCoord;
+        private SMALL_RECT region;
 
         public Win32Console(int width, int height) : base(width, height) {
-            Buffer = Array<Win32.CHAR_INFO>.Empty(width * height);
-            PrevBuffer = Array<Win32.CHAR_INFO>.Empty(width * height);
-            Handle = Win32.GetStdHandle(Win32.STD_OUTPUT_HANDLE);
+            Buffer = Array<CHAR_INFO>.Empty(width * height);
+            PrevBuffer = Array<CHAR_INFO>.Empty(width * height);
+            Handle = GetStdHandle(STD_OUTPUT_HANDLE);
 
             Console.SetWindowSize(width, height);
             Console.SetBufferSize(width, height);
 
-            int mode = 0;
+            uint mode;
 
-            if (!Win32.GetConsoleMode(Handle, ref mode)) throw new Win32Exception();
+            if (!GetConsoleMode(Handle, out mode)) throw new Win32Exception();
 
-            mode |= Win32.ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+            mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
 
-            if (!Win32.SetConsoleMode(Handle, mode)) throw new Win32Exception();
+            if (!SetConsoleMode(Handle, mode)) throw new Win32Exception();
 
-            Win32.ReadConsoleOutput(Handle, Buffer, bufferSize, bufferCoord, ref region);
+            ReadConsoleOutput(Handle, Buffer, bufferSize, bufferCoord, ref region);
 
-            bufferSize = new Win32.COORD((short)Width, (short)Height);
-            bufferCoord = new Win32.COORD(0, 0);
-            region = new Win32.SMALL_RECT { Left = 0, Top = 0, Right = (short)(width - 1), Bottom = (short)(height - 1) };
+            bufferSize = new COORD((short)Width, (short)Height);
+            bufferCoord = new COORD(0, 0);
+            region = new SMALL_RECT { Left = 0, Top = 0, Right = (short)(width - 1), Bottom = (short)(height - 1) };
         }
 
         public override void Clear(ConsoleColor color) {
-            Win32.CHAR_INFO chr = EMPTY_CHAR;
+            CHAR_INFO chr = EMPTY_CHAR;
             chr.Attributes = (byte)((byte)color + ((byte)color << 4));
 
             Array.Fill(Buffer, chr);
         }
 
         public override void Draw() {
-            Win32.WriteConsoleOutput(Handle, Buffer, bufferSize, bufferCoord, ref region);
+            WriteConsoleOutput(Handle, Buffer, bufferSize, bufferCoord, ref region);
         }
 
         public override void DrawChar(char c, int x, int y, ConsoleColor foreground, ConsoleColor background) {
@@ -67,7 +63,7 @@ namespace neonrpg.IO.Impl {
             if (!(index >= 0 && index < Buffer.Length)) return;
 
             Buffer[index].Attributes = (byte)((byte)foreground + ((byte)background << 4));
-            Buffer[index].Char.AsciiChar = (byte)c;
+            Buffer[index].AsciiChar = c;
         }
 
         //TODO implement this
@@ -93,5 +89,71 @@ namespace neonrpg.IO.Impl {
                 return new T[size];
             }
         }
+
+        const int STD_OUTPUT_HANDLE = -11;
+        const int ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004;
+
+        [StructLayout(LayoutKind.Explicit)]
+        struct CHAR_INFO {
+            [FieldOffset(0)]
+            public char UnicodeChar;
+            [FieldOffset(0)]
+            public char AsciiChar;
+            [FieldOffset(2)]
+            public ushort Attributes;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct COORD {
+            public short X;
+            public short Y;
+
+            public COORD(short X, short Y) {
+                this.X = X;
+                this.Y = Y;
+            }
+        };
+
+        struct SMALL_RECT {
+            public short Left;
+            public short Top;
+            public short Right;
+            public short Bottom;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetStdHandle(
+            int nStdHandle
+        );
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool GetConsoleMode(
+            IntPtr hConsoleHandle,
+            out uint lpMode
+        );
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool SetConsoleMode(
+            IntPtr hConsoleHandle,
+            uint dwMode
+        );
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool ReadConsoleOutput(
+            IntPtr hConsoleOutput,
+            [Out] CHAR_INFO[] lpBuffer,
+            COORD dwBufferSize,
+            COORD dwBufferCoord,
+            ref SMALL_RECT lpReadRegion
+        );
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteConsoleOutput(
+            IntPtr hConsoleOutput,
+            CHAR_INFO[] lpBuffer,
+            COORD dwBufferSize,
+            COORD dwBufferCoord,
+            ref SMALL_RECT lpWriteRegion
+        );
     }
 }
